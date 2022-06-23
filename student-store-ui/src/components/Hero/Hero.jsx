b93ceef8d3c6042fa86ca858f609bcb889719f0b
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-06-23 15:56:20 -0700

    Hero with names according to the README

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -3,13 +3,12 @@ import "./Hero.css"
 export default function Hero() {
     return (
         <div className="hero">
-            <div className="hero-text">
+            <div className="intro">
                 <h1>Welcome !</h1>
                 <h3>Yes, this is a store, the best you could find, trust me</h3>
             </div>
-            <div className="hero-img">
-                <img src="https://codepath-student-store-demo.surge.sh/assets/student_store_icon.18e5d61a.svg" alt="welcome" />
-            </div>
+            <img className="hero-img" src="https://codepath-student-store-demo.surge.sh/assets/student_store_icon.18e5d61a.svg" alt="welcome"  />
+
         </div>
     )
 }
