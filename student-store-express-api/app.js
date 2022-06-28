0ff4e399e8d0dd2a4e8fae139527ec6549924906
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-06-28 11:33:21 -0700

    Error handling, 'ping pong' message at GET with 200 status code

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
+const { NotFoundError} = require('./utils/errors')
 
 const app = express()
 
@@ -9,7 +10,24 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 app.get('/', (req, res)=>{
-    res.status(200).send({ "ping": "pong" })
+    res.status(200).send({ "ping": "pongg" })
 })
+
+
+app.use((req, res, next)=>{
+    console.log('Nooooo')
+    next((new NotFoundError()));
+});
+
+app.use((error, req, res, next)=>{
+    const { status , message} = error;
+
+    const errorObject = {
+        status : status || 500,
+        message : message || "Something went wrong with de application"
+    }
+
+    res.status(status).send({ error : (errorObject)});
+});
 
 module.exports = app
